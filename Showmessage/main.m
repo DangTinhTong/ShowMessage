8763b0852d660131cd1e2eec69c16d6015d590d2
admin <dangtinhtong@gmail.com>
2017-10-11 00:52:43 +0700

    Initial Commit

---
+++ Showmessage/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Showmessage
+//
+//  Created by admin on 10/11/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
